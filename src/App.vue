bd8099e694623515d026474237cb5487053111a0
masterkudi <cudini.andrea@gmail.com>
2023-10-18 16:54:01 +0200

    aggiornamento

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,27 @@
 <script>
-	export default {
+import axios from 'axios';
 
-	}
+	export default {
+    data() {
+      return {
+        projects: [],
+        pagination: {}
+      };
+    },
+    methods: {
+      fetchData() {
+        axios.get('http://127.0.0.1:8000/api/projects')
+          .then((response) => {
+            console.log(response);
+          })
+      }
+    },
+    mounted() {
+      this.fetchData();
+    }
+	};
 </script>
 
+<template>
+  <h1>Hello from Vue!</h1>
+</template>
